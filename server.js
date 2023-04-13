b848a3ae7aa044abd72e507f543201a2362964f1
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2023-04-13 20:55:51 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+// const path = require("path");
+//routes
+app.use("/", express.static("home"));
+app.use("/contact", express.static("LoginRegisterPage"));
+app.use("/news", express.static("news"));
+app.use("/about", express.static("about"));
+app.use("/contact", express.static("contact"));
+app.use("/LoginRegisterpage", express.static("LoginRegisterPage"));
+
+//port listening
+app.listen(3000, () => {
+  console.log("running on port http://localhost:3000");
+});
